ff4cf44df9c56e2d444b8ecd0628d1717b39e95d
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2023-10-09 13:32:12 +0200

    esqueleto

---
+++ pages/index.vue
@@ -0,0 +1,104 @@
+<template>
+ <div class="curved-div first">
+  <h1 class="mainh1">Covering letter generator</h1>
+  <p>
+    Generate a covering letter from your resume and the desired job offer.
+  </p>
+  <input type="button" value="Start Now" class="start">
+  <svg viewBox="0 0 1440 319">
+    <path fill="#00F5D4" fill-opacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+  </svg>
+</div>
+<div class="curved-div second">
+  <h1>Covering letter generator</h1>
+  <p>
+    Generate a covering letter from your resume and the desired job offer.
+  </p>
+
+  <svg viewBox="0 0 1440 319">
+    <path fill="#FEE440" fill-opacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+  </svg>
+</div>
+<div class="curved-div third">
+  <h1>Covering letter generator</h1>
+  <p>
+    Generate a covering letter from your resume and the desired job offer.
+  </p>
+  <svg viewBox="0 0 1440 319">
+    <path fill="#00BBF9" fill-opacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+  </svg>
+</div>
+<section class="footer">
+    <p>2023 by Franky Melero</p>
+</section>
+  </template>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    overflow-x: hidden;
+    
+}
+.curved-div {
+  background: #00BBF9;
+  color: black;
+  text-align: center;
+  border-bottom: transparent;
+  min-height: 75vh;
+  margin-top: -7vh;
+}
+.first{
+    min-height: 85vh;
+padding-top: 15vh;
+}
+
+.second{
+    background: #00F5D4 !important;
+}
+.third{
+    background: #FEE440 !important;
+}
+
+
+.curved-div h1 {
+  font-size: 6rem;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.curved-div p {
+  font-size: 2rem;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+.curved-div svg {
+  display: block;
+margin-top: 20vh;
+}
+.first svg{
+    display: block;
+margin-top: 10vh !important;
+}
+.start{
+    height: 60px;
+    width: 200px;
+    background-color: rgba(0,0,0,0.8);
+    color: white;
+    border:2px solid white;
+    margin-top: 10vh;
+    font-size: 26px;
+    border-radius: 15px;
+    transition: 0.2s;
+}
+.start:hover{
+    cursor: pointer;
+    background-color: rgba(0,0,0,1);
+    transition: 0.2s;
+}
+.footer{
+    text-align: center;
+    font-weight: 600;
+    font-size: 25px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: #00BBF9;
+    margin-top: -10vh;
+}
+</style>
